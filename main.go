32e4dc4598219a305460ff3af82470b6f68245e8
Feriyal <feriyalbinyahya@gmail.com>
2025-08-29 02:33:01 +0700

    menambahkan cors

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func InitializeApp() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://simpel-warehouse-firriyal.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
